d1959d27d2d82ea80b2035cbadc1bdfb1b9f13e9
trithangggggggg <trithang2006vn@gmail.com>
2025-06-17 13:32:10 +0700

    update

--- PTIT_CNTT4_IT201_Session01/PTIT_CNTT4_IT201_Session01_Bai07.c
+++ PTIT_CNTT4_IT201_Session01/PTIT_CNTT4_IT201_Session01_Bai07.c
@@ -3,39 +3,32 @@
 //
 #include <stdio.h>
 
-int loop(int arr[], int len) {   // do phuc tap: O(n^2)
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] == arr[j]) return 1;
+int checkDup(int numbers[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 1; j < size; j++) {
+            if (numbers[i] == numbers[j]) {
+                return 1;
+            }
         }
-        return 0;
-    }
-}
-
-int count(int arr[], int len) {     // do phuc tap: O(n)
-    int freq[1001] = {0};
-    for (int i = 0; i < len; i++) {
-        if (freq[arr[i]]) return 1;
-        freq[arr[i]] = 1;
     }
     return 0;
 }
 
+int checkDupSubArray(int numbers[], int size) {
+    // tao mang phu de kiem tra phan tu trung lap
+    int seeNumbers[1001];
+    // xet tung phan tu
+    for (int i = 0; i < size; i++) {
+        // kiem tra xem phan tu duoc xet da duoc tim thay chua
+        // neu da tung duoc tim thay thi tra ve gia tri la 1
+        if (seeNumbers[numbers[i]] == 1) {
+            // neu khong co phan tu nao trung lap thi tra ve gia tri 0
+            return 1;
+        }
+    }
+}
+
+
 int main() {
-    int data[] = {1, 3, 4, 5, 7, 2, 6};
-    int size = sizeof(data)/ sizeof(data[0]);
-
-    if (loop(data, size)) {
-        printf("loop: duplicated");
-
-    }else {
-        printf("loop: unique");
-    }
-
-    if (count(data,size)) {
-        printf("count: duplicated");
-    }else {
-        printf("count: unique");
-    }
     return 0;
 }
