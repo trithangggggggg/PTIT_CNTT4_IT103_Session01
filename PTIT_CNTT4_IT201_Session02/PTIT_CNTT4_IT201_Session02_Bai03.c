363dff6d1a12c3e960c0950ab83460a046b66fcc
trithangggggggg <trithang2006vn@gmail.com>
2025-06-19 02:04:22 +0700

    up

--- PTIT_CNTT4_IT201_Session02/PTIT_CNTT4_IT201_Session02_Bai03.c
+++ PTIT_CNTT4_IT201_Session02/PTIT_CNTT4_IT201_Session02_Bai03.c
@@ -1,3 +1,29 @@
 //
 // Created by ROG on 6/17/2025.
 //
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[100];
+
+    printf("Moi nhaap so luong phan tu (1-100):");
+    scanf("%d", &n);
+
+    printf("Moi nhap %d phan tu: \n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Mang dao nguoc la: \n [");
+    for (int i = n - 1; i >= 0;i--) {
+        printf("%d",arr[i]);
+        if (i != 0) {
+            printf(", ");
+        }
+    }
+    printf("] \n");
+
+
+    return 0;
+}
--- PTIT_CNTT4_IT201_Session02/PTIT_CNTT4_IT201_Session02_Bai04.c
+++ PTIT_CNTT4_IT201_Session02/PTIT_CNTT4_IT201_Session02_Bai04.c
@@ -1,3 +1,42 @@
 //
 // Created by ROG on 6/17/2025.
 //
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+    int position, newValue;
+
+    printf("Nhap so luong phan tu (1-100): ");
+    scanf("%d", &n);
+
+    printf("Nhap %d phan tu: \n",n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Moi nhap vi tri muon sua (0-%d): ",n -1);
+    scanf("%d", &position);
+
+    printf("Moi nhap gia tri moi: ");
+    scanf("%d", &newValue);
+
+    if (position >= 0 && position < n) {
+        arr[position] = newValue;
+    } else {
+        printf("Vi tri khong hop le \n");
+        return 1;
+    }
+    printf("Mang sau khi da cap nhat: \n [ ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+        if (i != n - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+
+
+    return 0;
+}
+
