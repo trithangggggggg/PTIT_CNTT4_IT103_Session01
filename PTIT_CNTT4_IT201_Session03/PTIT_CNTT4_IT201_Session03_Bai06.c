c68bbb8e386e03904846f485760872afb30eaf7f
trithangggggggg <trithang2006vn@gmail.com>
2025-06-24 22:14:32 +0700

    up

--- PTIT_CNTT4_IT201_Session03/PTIT_CNTT4_IT201_Session03_Bai06.c
+++ PTIT_CNTT4_IT201_Session03/PTIT_CNTT4_IT201_Session03_Bai06.c
@@ -1,3 +1,39 @@
 //
 // Created by ROG on 6/24/2025.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, m;
+    printf("Moi nhap so luong phan tu (1-1000): ");
+    scanf("%d", &n);
+    if (n <= 0 || n > 1000) {
+        printf("So luong phan tu khong hop le! \n");
+        return 1;
+    }
+    int *arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        printf("Moi nhap phan tu thu %d cho mang: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Moi ban nhap so phan tu muon them (m): ");
+    scanf("%d", &m);
+    arr = (int *)realloc(arr, (n + m) * sizeof(int));
+    for (int i = n; i < n + m; i++) {
+        printf("Moi ban nhap phan tu thu %d cho mang: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Mang hoan thien: \n[");
+    for (int i = 0; i < n + m; i++) {
+        printf("%d", arr[i]);
+        if (i < n + m - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+    free(arr);
+
+
+    return 0;
+}
--- PTIT_CNTT4_IT201_Session03/PTIT_CNTT4_IT201_Session03_Bai07.c
+++ PTIT_CNTT4_IT201_Session03/PTIT_CNTT4_IT201_Session03_Bai07.c
@@ -1,3 +1,66 @@
 //
 // Created by ROG on 6/24/2025.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
+void inputMatrix(int matrix[10][10], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("matrix[%d][%d] = ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void showMatrix(int matrix[10][10], int rows, int cols) {
+    printf("Ma tran: \n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+int  sum(int matrix[10][10], int rows, int cols) {
+    int k, total = 0;
+    printf("Nhap chi so muon tinh tong: ");
+    scanf("%d", &k);
+    if (k < 0 || k >= rows) {
+        printf("Mang can tinh khong ton tai!\n");
+        return -1;
+    }
+    for (int j = 0; j < cols; j++) {
+        total += matrix[k][j];
+    }
+    return total;
+}
+
+int main() {
+    int matrix[10][10];
+    int rows, cols;
+
+    printf("Nhap so hang: ");
+    scanf("%d", &rows);
+    if (rows <= 0 || rows >= 1000) {
+        printf("So hang khong hop le! \n");
+        return 1;
+    }
+    printf("Nhap so cot: ");
+    scanf("%d", &cols);
+    if (cols <= 0 || cols >= 1000) {
+        printf("So cot khong hop le! \n");
+        return 1;
+    }
+
+    inputMatrix(matrix, rows, cols);
+    showMatrix(matrix, rows, cols);
+    int result = sum(matrix, rows, cols);
+    if (result != -1) {
+        printf("Sum = %d", result);
+    }
+
+
+
+    return 0;
+}
